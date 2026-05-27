fe3d4f53f9231ef6f8bafcbe7972974b194488f8
Kabir <54203004+KabirNasir@users.noreply.github.com>
2026-05-27 17:01:24 +0530

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if t in s:
+            return t
+        if len(s) < len(t):
+            return ""
+        c1 = {}
+        for i in range(len(t)):
+            c1[t[i]]=1 + c1.get(t[i],0)
+        l = 0
+        match = 0
+        window = {}
+        minStr = ""
+        minStrlen = float('inf')
+        for r in range(len(s)):
+            window[s[r]] = 1 + window.get(s[r], 0)
+            if(s[r] in c1 and window[s[r]] == c1[s[r]] ):
+                match+=1
+            while match == len(c1):
+                if (r - l + 1) < minStrlen:
+                    minStr = s[l:r+1]
+                    minStrlen = r - l + 1
+                window[s[l]] -=1
+                if s[l] in c1 and window[s[l]] < c1[s[l]]:
+                    match -= 1
+                l += 1
+            
+        return minStr
+        
